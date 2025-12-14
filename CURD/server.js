bf58b0bdf1a4c9beef3cde36d5e640add206c51b
dikeshchouha1 <dikeshchouhan02@gmail.com>
2025-12-14 20:15:50 +0530

    put and delete route

--- CURD/server.js
+++ CURD/server.js
@@ -5,6 +5,9 @@ const app=express();
 const  PORT=3000;
 
 app.use(express.json())
+
+
+// ✅ Create (POST)
 app.post('/user',async(req,res)=>{
     try{
         const user=await User.create(req.body);
@@ -17,6 +20,8 @@ app.post('/user',async(req,res)=>{
     }
    
 })
+
+// ✅ Read All (GET)
 app.get("/users", async (req, res) => {
  
   try{
@@ -28,6 +33,42 @@ app.get("/users", async (req, res) => {
     }
 });
 
+
+// ✅ Read One (GET)
+app.get("/users/:id",async (req,res)=>{
+    try{
+        const user= await User.findById(req.params.id);
+        res.json(user)
+      
+    }
+    catch(err){
+        console.log(err)
+    }
+})
+
+
+// ✅ Update (PUT)
+app.put("/users/:id",async (req,res)=>{
+    try{
+      const update=await User.findByIdAndUpdate(req.params.id,req.body,{
+        new:true,
+      })
+      res.json(update);
+    }
+    catch(err){
+        console.log(err)
+    }
+})
+
+// ✅ Delete (DELETE)
+app.delete("/users/:id",async(req,res)=>{
+    try{
+      await User.findByIdAndDelete(req.params.id)
+      res.json({message:"User deleted"})
+    }catch(err){
+        console.log(err)
+    }
+})
 app.listen(PORT,()=>{
     connectDB();
     console.log(`server is runing ${PORT}`)
